b32f5cc8a476dd733d2d9a32ced4fe3856df9a39
Nicole MN.E <mcnultyengstrom@gmail.com>
2024-05-24 22:21:23 +0000

    add gameserializer

---
+++ games/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.db import IntegrityError
+from .models import Game
+
+class GameSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Game model
+    The create method handles the unique constraint on 
+    'title', 'developer', 'genre', and 'platform'
+    """
+    average_star_rating = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Game
+        fields = [
+            'id', 'title', 'developer', 'genre', 'platform', 'average_star_rating'
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'A game with these details already exists.'
+            })
+
+    def validate_title(self, value):
+        if len(value) > 255:
+            raise serializers.ValidationError('Title length exceeds 255 characters')
+        return value
