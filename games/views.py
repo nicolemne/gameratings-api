ea3622b9e179972fcec5595ab7e2b8501527654d
Nicole MN.E <mcnultyengstrom@gmail.com>
2024-05-24 22:42:00 +0000

    add gamelist and gamedetail generic views

--- games/views.py
+++ games/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from gameratings.permissions import IsOwnerOrReadOnly
+from .models import Game
+from .serializers import GameSerializer
 
-# Create your views here.
+
+class GameList(generics.ListCreateAPIView):
+    """
+    List games or add a game when creating a post, if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = GameSerializer
+    queryset = Game.objects.all()
+
+class GameDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a game instance.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = GameSerializer
+    queryset = Game.objects.all()
--- likes/serializers.py
+++ likes/serializers.py
@@ -6,7 +6,8 @@ from likes.models import Like
 class LikeSerializer(serializers.ModelSerializer):
     """
     Serializer for the Like model
-    The create method handles the unique constraint on 'owner' and 'post'
+    The create method handles the unique constraint on
+    'owner' and 'post'
     """
     owner = serializers.ReadOnlyField(source='owner.username')
 
