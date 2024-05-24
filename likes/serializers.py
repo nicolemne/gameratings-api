6b05d66c3b5a575f81fb34ccc293ec3909e8c36d
Nicole MN.E <mcnultyengstrom@gmail.com>
2024-05-24 18:05:54 +0000

    add likelist and likedetail generic views

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,6 +1,6 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from likes.models import Like
 
 
 class LikeSerializer(serializers.ModelSerializer):
