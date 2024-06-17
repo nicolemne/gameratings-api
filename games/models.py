ac22e76fd884e89c21cb999e806cdc26b9d1236f
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-06-17 16:43:15 +0000

    update game model and add genre app & model

--- games/models.py
+++ games/models.py
@@ -6,32 +6,19 @@ class Game(models.Model):
     Game model that stores information about the games
     that are reviewed. Generates a URL-slug from the title.
     """
-    GENRE_CHOICES = [
-        ('Action', 'Action'),
-        ('Adventure', 'Adventure'),
-        ('RPG', 'Role-Playing Game'),
-        ('Strategy', 'Strategy'),
-        ('Simulation', 'Simulation'),
-        ('Sports', 'Sports'),
-    ]
-
-    PLATFORM_CHOICES = [
-        ('PC', 'PC'),
-        ('PS4', 'PlayStation 4'),
-        ('XBOX', 'Xbox One'),
-        ('SWITCH', 'Nintendo Switch'),
-    ]
-
     title = models.CharField(max_length=255)
-    developer = models.CharField(max_length=255)
-    genre = models.CharField(max_length=50, choices=GENRE_CHOICES)
-    platform = models.CharField(max_length=50, choices=PLATFORM_CHOICES)
+    platform = models.ForeignKey(
+        'platforms.Platform', on_delete=models.CASCADE, related_name='games'
+        )
+    genre = models.ForeignKey(
+        'genres.Genre', on_delete=models.CASCADE, related_name='games'
+        )
     average_star_rating = models.FloatField(default=0.0)
     slug = models.SlugField(max_length=255, unique=True, blank=True)
 
     class Meta:
         unique_together = (
-            'title', 'developer', 'genre', 'platform'
+            'title', 'genre', 'platform'
         )
 
     def save(self, *args, **kwargs):
--- platforms/models.py
+++ platforms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Platform(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    developer = models.CharField(max_length=255)
+    release_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.names
