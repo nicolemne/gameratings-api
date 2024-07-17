e63240637376d41b9a623a2ae0276ec180522f99
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-07-17 13:02:27 +0000

    add admin view to profile model

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    '''
+    Customises the Profile model admin view to display a list of fields, search and filters
+    ''' 
+    list_display = ('owner', 'created_at', 'updated_at', 'name')
+    search_fields = ('owner__username', 'name')
+    list_filter = ('created_at', 'updated_at', 'owner')
+    
+
