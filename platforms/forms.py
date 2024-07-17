5013f2d1bca3c5c25951126d475c1c2f141bedc4
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-07-17 16:38:55 +0000

    update platforms forms.py DateField to only display year

--- platforms/forms.py
+++ platforms/forms.py
@@ -3,11 +3,14 @@ from .models import Platform
 
 class PlatformForm(forms.ModelForm):
     '''
-    Override the release_date field to use a DateInput widget 
-    to display only the date in the admin interface
+    Override the release_date field to use a NumberInput widget 
+    to display only the year in the admin interface
     '''
     release_date = forms.DateField(
-        widget=forms.DateInput(attrs={'type': 'date'})
+        widget=forms.NumberInput(attrs={
+            'type': 'number', 'min': '1900', 'max': '2100', 'step': '1', 'placeholder': 'YYYY'
+            }),
+        input_formats=['%Y']
     )
 
     class Meta:
