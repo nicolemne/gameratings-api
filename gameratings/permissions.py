a8c4397753ee6b8ab3719b2a97d94c24f6bf0f99
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-07-19 20:48:36 +0000

    add IsUserOrReadOnly class to permissions.py

--- gameratings/permissions.py
+++ gameratings/permissions.py
@@ -31,3 +31,14 @@ class IsAdminOrOwnerOrReadOnly(permissions.BasePermission):
             return obj.posts.filter(owner=request.user).exists()
         
         return False
+    
+
+class IsUserOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow users to view, edit, 
+    or delete their saved games.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
