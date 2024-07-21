5f04425bfb66adb857a0d9c4122f7b0bc06b2ad2
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-07-21 16:01:12 +0000

    setup ALLOWED_HOST and CLIENT_ORIGIN_DEV

--- gameratings/settings.py
+++ gameratings/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import re
 import dj_database_url
 
 if os.path.exists('env.py'):
@@ -37,10 +38,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'), 
     'localhost', 
     'https://nicolemne-gameratingsap-snp7effbtaz.ws.codeinstitute-ide.net/',
     '8000-nicolemne-gameratingsap-snp7effbtaz.ws.codeinstitute-ide.net',
-    'gameratings-api-d04888e8239b.herokuapp.com'
     ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -102,9 +103,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
     
 CORS_ALLOW_CREDENTIALS = True
