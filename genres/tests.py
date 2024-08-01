3663948698374b78b0ba582295aeb39fb09584a1
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-08-01 17:05:37 +0000

    add GenreListViewTests

--- genres/tests.py
+++ genres/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from .models import Genre
 
-# Create your tests here.
+
+class GenreListViewTests(APITestCase):
+    """
+    Tests: Genre list view
+    """
+
+    def setUp(self):
+        User.objects.create_user(username="nicole", password="blackcat")
+        Genre.objects.create(
+            name="Test Genre",
+        )
+
+    def test_can_list_genres(self):
+        response = self.client.get("/genres/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["results"]), 1)
+        self.assertEqual(response.data["results"][0]["name"], "Test Genre")
