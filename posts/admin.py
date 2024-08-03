929baafc5cb6d81b708ca3867d41936b52211bc1
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-08-03 15:18:37 +0000

    fix PostForm import

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Post
-from .forms import PostAdminForm
+from .forms import PostForm
 
 
 @admin.register(Post)
@@ -9,7 +9,7 @@ class PostAdmin(admin.ModelAdmin):
     Customizes the Post admin view to display, search, and filter posts.
     """
 
-    form = PostAdminForm
+    form = PostForm
     list_display = ("title", "owner", "game", "created_at", "updated_at", "star_rating")
     search_fields = ("title", "owner__username", "game__title")
     list_filter = ("created_at", "updated_at", "game", "owner", "star_rating")
