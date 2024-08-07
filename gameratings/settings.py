bf150ea8ed82276503cf674faa021fcd8535f981
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-08-07 23:51:07 +0000

    fix cors origins in settings.py

--- gameratings/settings.py
+++ gameratings/settings.py
@@ -107,9 +107,8 @@ if 'CLIENT_ORIGIN' in os.environ:
     ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
+    
+    CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.codeinstitute-ide\.net$",]
     
 CORS_ALLOW_CREDENTIALS = True
 
