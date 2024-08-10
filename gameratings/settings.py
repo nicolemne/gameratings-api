6953ba950005043ae91db43b18757a680882b72e
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-08-10 18:19:30 +0000

    add console.log to debug CORS issue

--- gameratings/settings.py
+++ gameratings/settings.py
@@ -104,10 +104,10 @@ if "CLIENT_ORIGIN_DEV" in os.environ:
     extracted_url = re.match(
         r"^.+-", os.environ.get("CLIENT_ORIGIN_DEV", ""), re.IGNORECASE
     ).group(0)
-
     CORS_ALLOWED_ORIGIN_REGEXES = [
         r"^https://.*\.codeinstitute-ide\.net$",
     ]
+    print("CORS_ALLOWED_ORIGIN_REGEXES:", CORS_ALLOWED_ORIGIN_REGEXES)
 
 CORS_ALLOW_CREDENTIALS = True
 
