9118fac1c6999011378a052ce3dd2c2fa4c52d51
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-08-17 02:49:48 +0000

    add all game fields to saved_games Serializer

--- saved_games/serializers.py
+++ saved_games/serializers.py
@@ -8,11 +8,16 @@ class SavedGameSerializer(serializers.ModelSerializer):
     """
 
     user = serializers.ReadOnlyField(source="user.username")
+
+    game_id = serializers.IntegerField()
     game_title = serializers.ReadOnlyField(source="game.title")
-    game_developer = serializers.ReadOnlyField(source="game.game_developer")
     game_platform = serializers.ReadOnlyField(source="game.platform.name")
+    game_image = serializers.ImageField(source="game.image", read_only=True)
+    game_developer = serializers.ReadOnlyField(source="game.game_developer")
+    game_release_year = serializers.SerializerMethodField(source="game.release_year")
     game_genre = serializers.ReadOnlyField(source="game.genre.name")
     game_multiplayer = serializers.ReadOnlyField(source="game.multiplayer")
+    game_average_star_rating = serializers.SerializerMethodField()
 
     def validate(self, data):
         """
@@ -28,17 +33,41 @@ class SavedGameSerializer(serializers.ModelSerializer):
                 )
         return data
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError("Image size is larger than 2 MB")
+        if value.image.width > 4096:
+            raise serializers.ValidationError("Image width larger than 4096 px")
+        if value.image.height > 4096:
+            raise serializers.ValidationError("Image height larger than 4096 px")
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context["request"]
+        return request.user == obj.owner
+
+    def get_game_release_year(self, obj):
+        return obj.game.release_year.year if obj.game.release_year else "N/A"
+
+    def get_game_average_star_rating(self, obj):
+        if obj.game and hasattr(obj.game, "average_star_rating"):
+            return round(obj.game.average_star_rating, 1)
+        return None
+
     class Meta:
         model = SavedGame
         fields = [
             "id",
             "user",
-            "game",
-            "game_title",
             "created_at",
             "status",
-            "game_developer",
+            "game_id",
+            "game_title",
             "game_platform",
+            "game_image",
+            "game_developer",
+            "game_release_year",
             "game_genre",
             "game_multiplayer",
+            "game_average_star_rating",
         ]
