dabcb10503b15c88aa4506883c9825c718d051b2
Nicole McNulty Engström <mcnultyengstrom@gmail.com>
2024-08-17 02:51:01 +0000

    remove image validation and get owner from saved_game Serializer

--- saved_games/serializers.py
+++ saved_games/serializers.py
@@ -33,19 +33,6 @@ class SavedGameSerializer(serializers.ModelSerializer):
                 )
         return data
 
-    def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:
-            raise serializers.ValidationError("Image size is larger than 2 MB")
-        if value.image.width > 4096:
-            raise serializers.ValidationError("Image width larger than 4096 px")
-        if value.image.height > 4096:
-            raise serializers.ValidationError("Image height larger than 4096 px")
-        return value
-
-    def get_is_owner(self, obj):
-        request = self.context["request"]
-        return request.user == obj.owner
-
     def get_game_release_year(self, obj):
         return obj.game.release_year.year if obj.game.release_year else "N/A"
 
